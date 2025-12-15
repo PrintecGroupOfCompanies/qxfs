072a35b85f813e82619b6b5adf49ce2a9630d77c
Ivaylo Dimitrov <i.dimitrov@printecgroup.com>
2025-12-15 11:37:10 +0200

    define QXFS_EXPORT only if not already defined
    Signed-off-by: Ivaylo Dimitrov <i.dimitrov@printecgroup.com>

--- qxfs_global.h
+++ qxfs_global.h
@@ -3,10 +3,12 @@
 
 #include <QtCore/qglobal.h>
 
-#if defined(QXFS_LIBRARY)
-#  define QXFS_EXPORT Q_DECL_EXPORT
-#else
-#  define QXFS_EXPORT Q_DECL_IMPORT
+#if !defined(QXFS_EXPORT)
+#  if defined(QXFS_LIBRARY)
+#    define QXFS_EXPORT Q_DECL_EXPORT
+#  else
+#    define QXFS_EXPORT Q_DECL_IMPORT
+#  endif
 #endif
 
 #endif // QXFS_GLOBAL_H
